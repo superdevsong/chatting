84a303e200137a4bd2be1a60303b14656f0207f2
rainingstarr <122976349+rainingstarr@users.noreply.github.com>
2023-03-02 19:25:10 +0900

    add index,join,main.html superdevsong/chatting#3.

---
+++ chattingProject/src/main/resources/static/web/js/common.js
@@ -0,0 +1,37 @@
+$(function(){
+    $("body > div").fadeIn(500,function(){
+        $(this).animate({
+			"opacity":"1",
+            "margin-top": "0"
+        },1000);
+    });
+    
+    $("a.sign").click(function(){
+        var url = $(this).attr("href");
+        $("body > div").animate({
+            "opacity": "0",
+            "margin-top": "-100px"
+        },500,function(){
+            document.location.href = url;
+        });        
+        return false;
+    });	
+
+    $(".txtPW_box img").click(function(){
+		$(".txtPW_box").each(function(){
+			var $this = $(this).children("img");
+			var input = $(this).children("input");
+			if(input.attr("type") == "password"){
+				$this.attr("src","img/icon_eye_02.png");
+				input.attr("type","text");
+			}else{
+				$this.attr("src","img/icon_eye_01.png");
+				input.attr("type","password");
+			}
+			
+		});
+    });
+});
+
+$(window).load(function(){
+});
