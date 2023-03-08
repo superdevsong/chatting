cee57daa7f770e1b588b88aa8e77b25850386973
이형우 <dlguddn66@naver.com>
2023-03-08 14:14:38 +0900

    Update common.js

--- chattingProject/src/main/resources/static/web/js/common.js
+++ chattingProject/src/main/resources/static/web/js/common.js
@@ -16,33 +16,24 @@ $(function(){
         });        
         return false;
     });	
-
-    $(".txtPW_box img").click(function(){
-		$(".txtPW_box").each(function(){
-			var $this = $(this).children("img");
-			var input = $(this).children("input");
-			if(input.attr("type") == "password"){
-				$this.attr("src","/web/img/icon_eye_02.png");
-				input.attr("type","text");
-			}else{
-				$this.attr("src","/web/img/icon_eye_01.png");
-				input.attr("type","password");
-			}
-			
-		});
-    });
 });
 
 window.addEventListener('DOMContentLoaded', function(){
-    const imgs = document.querySelectorAll('.txtPW_box img');
-    imgs.forEach(function(){
-        this.addEventListener('click',function(){
-            alert('hi');
+    var pwImg = document.querySelectorAll('.txtPW_box img');
+    pwImg.forEach(function(element,index){
+        element.addEventListener('click',function(){
+            var input = document.querySelectorAll('.txtPW_box input');
+            if(input[0].type == 'password'){
+                for(i=0;i<pwImg.length;i++){
+                    pwImg[i].src = '/web/img/icon_eye_02.png'
+                    input[i].type = 'text'
+                }
+            }else{
+                for(i=0;i<pwImg.length;i++){
+                    pwImg[i].src = '/web/img/icon_eye_01.png'
+                    input[i].type = 'password'
+                }
+            }
         });
     });
-    // items.forEach(item => {
-    // item.addEventListener('click', e => {
-    //     item.classList.toggle('active')
-    // })
-    // })
 });
